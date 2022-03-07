c064ee1998ca8e8ac8305e51a85ce3d697352080
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-03-07 19:34:00 +0100

    feat(router): add router and links in navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import Navigation from "./common/components/navigation";
+import Navigation from "./common/components/Navigation";
 import Paper from "@mui/material/Paper";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <Box className="App">
-            <Paper
+        <Box className="App" sx={{p: 2}}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<h1>Catálogo</h1>} />
+                    <Route path="/mis-cursos" element={<h1>Mis Cursos</h1>} />
+                </Routes>
+                <Paper
                 sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
                 elevation={3}
             >
                 <Navigation />
             </Paper>
+            </BrowserRouter>
+            
         </Box>
     );
 }
--- src/common/components/navigation.tsx
+++ src/common/components/navigation.tsx
@@ -1,24 +1,35 @@
-import * as React from 'react';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import List from '@mui/icons-material/ListRounded';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import React, { useState } from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import List from "@mui/icons-material/ListRounded";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
-  const [value, setValue] = React.useState(0);
+    const [value, setValue] = useState(0);
 
-  return (
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Catálogo" icon={<List />} />
-        <BottomNavigationAction label="Mis Cursos" icon={<FavoriteIcon />} />
-      </BottomNavigation>
-  );
+    return (
+        <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(_, value) => {
+                setValue(value);
+            }}
+        >
+            <BottomNavigationAction
+                label="Catálogo"
+                icon={<List />}
+                component={Link}
+                to='/'
+            />
+            <BottomNavigationAction
+                label="Mis Cursos"
+                icon={<FavoriteIcon />}
+                component={Link}
+                to='/mis-cursos'
+            />
+        </BottomNavigation>
+    );
 };
 
 export default Navigation;
