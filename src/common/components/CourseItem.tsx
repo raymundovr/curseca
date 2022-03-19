673d45dbf6d078bff9d7dfb9ec67b8f26c741e6f
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2022-03-19 14:26:17 +0100

    feat(course): add button to add course to my courses

--- src/common/components/CourseItem.tsx
+++ src/common/components/CourseItem.tsx
@@ -1,25 +1,31 @@
-import { School } from '@mui/icons-material';
-import { Avatar, ListItem, ListItemAvatar, ListItemText } from '@mui/material';
-import React from 'react';
-import { Course } from '../types';
+import React from "react";
+import { AddBoxRounded } from "@mui/icons-material";
+import {
+    Avatar,
+    IconButton,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+} from "@mui/material";
+import { Course } from "../types";
 
 interface OwnProps {
     course: Course;
 }
 
-const CourseItem = ({ course }: OwnProps) => (<ListItem
-data-testid={`course-${course.id}`}
-key={course.id}
->
-<ListItemAvatar>
-    <Avatar>
-        <School color="primary" />
-    </Avatar>
-</ListItemAvatar>
-<ListItemText
-    primary={course.name}
-    secondary={course.description}
-/>
-</ListItem>);
+const CourseItem = ({ course }: OwnProps) => (
+    <ListItem data-testid={`course-${course.id}`} key={course.id}>
+        <ListItemText primary={course.name} secondary={course.description} />
+        <ListItemAvatar>
+            <IconButton
+                color="primary"
+                aria-label="add course"
+                data-testid={`add-course-${course.id}`}
+            >
+                <AddBoxRounded />
+            </IconButton>
+        </ListItemAvatar>
+    </ListItem>
+);
 
 export default CourseItem;
